05ee5dc55fc9539c3ea879124aebd99dec00c0e8
Delano Oliveira <delano.oliveira@embedded.ufcg.edu.br>
2015-05-14 19:32:34 -0300

    Fixing problems with Date in Widget

--- src/main/java/com/nanuvem/metagui/server/MetaGuiEntryPoint.java
+++ src/main/java/com/nanuvem/metagui/server/MetaGuiEntryPoint.java
@@ -46,14 +46,16 @@ public class MetaGuiEntryPoint {
 		Context fieldContext = createContext("field", WidgetType.Property, contextService);
 		Widget listingTableWidget = createWidget("ListingTable", WidgetType.EntitySet, readWidgetFile("ListingTable.js"), widgetService, propertyContext, formContext);
 		Widget toStringPropertyWidget = createWidget("ToStringProperty", WidgetType.Property, readWidgetFile("ToStringProperty.js"), widgetService);
-		Widget DateFormatterWidget = createWidget("DateFormatterWidget", WidgetType.Property, readWidgetFile("DateFormatterWidget.js"), widgetService);
+		Widget dateFormatterWidget = createWidget("DateFormatterWidget", WidgetType.Property, readWidgetFile("DateFormatterWidget.js"), widgetService);
 		Widget simpleFormWidget = createWidget("SimpleFormWidget", WidgetType.Entity, readWidgetFile("SimpleFormWidget.js"), widgetService, fieldContext);
 		Widget simpleTextFieldProperty = createWidget("SimpleTextFieldProperty", WidgetType.Property, readWidgetFile("SimpleTextFieldProperty.js"), widgetService);
+		Widget datePickerWidget = createWidget("DatePickerWidget", WidgetType.Property, readWidgetFile("DatePickerWidget.js"), widgetService);
 		createRule(rootContext, null, null, null, null, listingTableWidget, null, ruleService);
 		createRule(propertyContext, null, null, null, null, toStringPropertyWidget, null, ruleService);
-		createRule(propertyContext, null, PropertyTypeType.date, null, null, DateFormatterWidget, "{\"format\": \"dd-mm-yy\"}", ruleService);
+		createRule(propertyContext, null, PropertyTypeType.date, null, null, dateFormatterWidget, "{\"format\": \"dd-mm-yy\"}", ruleService);
 		createRule(formContext, null, null, null, null, simpleFormWidget, null, ruleService);
 		createRule(fieldContext, null, null, null, null, simpleTextFieldProperty, null, ruleService);
+		createRule(fieldContext, null, PropertyTypeType.date, null, null, datePickerWidget, null, ruleService);
 	}
 	
 	private static Context createContext(String name, WidgetType type, ContextService contextService) {
